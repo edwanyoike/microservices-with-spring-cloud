f326fad76101c990c5aebd31d1113248514a2690
Edwanyoike <eduwanyoike@gmail.com>
2020-12-20 00:15:52 +0300

    Init

---
+++ users/src/test/java/com/utility/users/UsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utility.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
