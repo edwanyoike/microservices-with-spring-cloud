417002b61301302f8bde84f29e3225d7a9166b87
Edwanyoike <eduwanyoike@gmail.com>
2020-12-21 00:13:54 +0300

    enable spring security on the API gateway ZUUL

---
+++ ZuulGateway/src/main/java/com/utility/ZuulGateway/security/WebSecurityConfig.java
@@ -0,0 +1,42 @@
+package com.utility.ZuulGateway.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    private Environment environment;
+
+    @Autowired
+    public WebSecurityConfig(Environment environment) {
+        this.environment = environment;
+    }
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
+        http.authorizeRequests()
+                .antMatchers(environment.getProperty("api.h2Console.url.path")).permitAll()
+                .antMatchers(HttpMethod.POST,environment.getProperty("api.registration.url.path")).permitAll()
+                .antMatchers(HttpMethod.POST,environment.getProperty("api.login.url.path")).permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .addFilter(new AuthorizationFilter(authenticationManager(),environment));
+
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+    }
+
+
+
+
+}
